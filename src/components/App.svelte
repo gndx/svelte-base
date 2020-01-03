58deaababc259d8f74b39005d0af0980794c30d8
Oscar Barajas Tavares <oscar@arepa.dev>
2020-01-02 19:26:21 -0500

    This commit is a lie

---
+++ src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  console.log("Svelte");
+</script>
+
+<style>
+  :global(body) {
+    background-color: #676778;
+    color: white;
+  }
+  .main {
+    display: flex;
+    justify-content: center;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello World</h1>
+</div>
